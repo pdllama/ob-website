773c1cce6c62eb1e24671387ba04fa815e667f83
Omar <omar_born@hotmail.com>
2026-05-19 16:22:26 -0400

    added tailwind and react router

--- src/browser.tsx
+++ src/browser.tsx
@@ -1,1 +1,10 @@
 
+import { createBrowserRouter } from "react-router";
+import Root from "./routes/root";
+
+const browser = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root/>
+    }
+])
